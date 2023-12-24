c3a6dd4fb0185e42358178705206e18eadb2b39c
Samsung <isupov_evgenii@mail.ru>
2023-12-24 19:23:49 +0300

    #1 написана функция сортировки массива с помощью алгоритма пузырьком

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def BoubleSort(arr:list) -> list:
-    pass
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 def Reverse(arr:list) -> list:
